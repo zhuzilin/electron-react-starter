a80ffa2ae89fa464982fdd409e00e4d0f29a2f21
zhuzilin <zhuzl14@mails.tsinghua.edu.cn>
2018-09-04 13:31:51 -0400

    step 3

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Game.css';
 
 class Square extends React.Component {
+    // TODO: remove the constructor
     constructor(props) {
         super(props);
         this.state = {
@@ -10,11 +11,10 @@ class Square extends React.Component {
     }
 
     render() {
+        // TODO: use onClick={this.props.onClick}
+        // TODO: replace this.state.value with this.props.value
         return (
-            <button
-                className="square"
-                onClick={() => this.setState({value: 'X'})}
-            >
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
                 {this.state.value}
             </button>
         );
@@ -22,8 +22,15 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return <Square value={this.state.squares[i]} />;
     }
 
     render() {
@@ -33,19 +40,13 @@ class Board extends React.Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
                 </div>
             </div>
         );
@@ -67,4 +68,5 @@ class Game extends React.Component {
         );
     }
 }
+
 export default Game;
