211415812b4d3e355a10cf284ed4c5e9cb211980
zhuzilin <zhuzl14@mails.tsinghua.edu.cn>
2018-09-06 10:48:01 -0400

    redux thunk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import './index.css';
-import App from './components/App';
+import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Root />,
     document.getElementById('root')
 );
 registerServiceWorker();
+
